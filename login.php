85158a012e48d1b39fdea294297ca2cfedb3ee6c
Chung Nguyen Ha <nguyen.ha.chung@captcha.com>
2019-07-08 16:53:54 +0900

    auth

---
+++ login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form action="session-based-auth.php" method="post">
+		<input type="text" name="usernam">
+		<input type="password" name="password">
+		<input type="submit" name="" value="submit">
+	</form>
+</body>
+</html>
